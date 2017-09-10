7da0f42ea90fcbba7bdd815d211f0d8475573ef5
Scottie Schneider <scott@laclave.co>
2017-09-10 18:53:18 -0500

    Commit 3
    ✅ Added menu buttons
    ✅ Added check for OS, in order to add the filler object needed for the menu
    ✅ Added quit functionality

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 const electron = require('electron');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
 
 app.on('ready', () => {
 	mainWindow = new BrowserWindow({});
 	mainWindow.loadURL(`file://${__dirname}/main.html`);
+	// Create the menu from the template below
+	const mainMenu = Menu.buildFromTemplate(menuTemplate);
+	Menu.setApplicationMenu(mainMenu);
 });
+
+// Create menu template
+const menuTemplate = [
+	{
+		label: 'File', 
+		submenu: [
+			{
+				label: 'New Todo'
+			},
+			{
+				label: 'Quit',
+				click(){
+					app.quit();
+				}
+			}
+		]
+	}
+];
+// Checking to see if it's a mac OS we're on, to add the menu option filler object
+if(process.platform === 'darwin'){
+	menuTemplate.unshift({});
+}
