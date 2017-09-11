66abcd3cc0c9b9e4e76269257da248731b4c6d31
Scottie Schneider <scott@laclave.co>
2017-09-10 19:10:10 -0500

    Commit 5 ✅ Add menu functionality to create a new window ✅ Make menu smaller by passing in the options object to BrowserWindow

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const electron = require('electron');
 const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
+let addWindow;
 
 app.on('ready', () => {
 	mainWindow = new BrowserWindow({});
@@ -12,13 +13,23 @@ app.on('ready', () => {
 	Menu.setApplicationMenu(mainMenu);
 });
 
+function createAddWindow() {
+	addWindow = new BrowserWindow({
+		width: 300,
+		height: 200,
+		title: 'Add New Todo'
+	});
+}
 // Create menu template
 const menuTemplate = [
 	{
 		label: 'File', 
 		submenu: [
 			{
-				label: 'New Todo'
+				label: 'New Todo',
+				click() {
+					createAddWindow();
+				}
 			},
 			{
 				label: 'Quit',
